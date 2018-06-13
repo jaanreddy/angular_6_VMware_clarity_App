7dfd636304c140c84bb2a232a7da9c74268941c7
Jaanreddy Depa <jdepa@jdepa-a01.vmware.com>
2018-06-13 15:02:01 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ClarityModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
